9b3c08322fecf4ec184e9bd5430957ab61de12ed
KwonSanta <KwonSanta@gmail.com>
2024-05-09 16:14:16 +0900

    배열 map 메소드로 배열의 각 원소를 컴포넌트로 매핑 할 수 있음 (자주 일어남)

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
 import React from "react";
 
 function App(props) {
-  // 배열 메소드
-  const a = [];
-
-  //--[1] push : 새 값을 배열 맨 뒤에 추가
-  a.push("흥민");
-  a.push("강인");
-  a.push(3.14);
-  console.log(a);
-
-  //--[2] pop : 맨 뒤의 값을 제거 후 리턴
-  let val1 = a.pop();
-  console.log(val1); // 3.14
-  let val2 = a.pop();
-  console.log(val2); // "강인"
-  console.log(a); // 흥민
-
-  //--[3] map : 각 원소를 다른 값으로 매핑(mapping) 후 새 배열 리턴
-  const b = [5, 4, 3, 2, 1];
-  console.log(b);
-
-  function action(param) {
-    return param * 2;
-  }
-  const c = b.map(action);
-  console.log("b", b);
-  console.log("c", c);
-
-  const d = b.map(function (param) {
-    return param * 3;
+  const a = ["흥민", "강인", "희찬"];
+  const b = a.map(function (item) {
+    return item + "선수";
   });
-  console.log("d", d);
-  return <div></div>;
+  const c = a.map(function (item) {
+    return <li>{item}</li>;
+  });
+  return (
+    <div>
+      <div>{a}</div>
+      <div>
+        {a[0]}
+        {a[1]}
+        {a[2]}
+      </div>
+      <div>{b}</div>
+      <div>{c}</div>
+      <div>
+        {" "}
+        {/* 축약 1.*/}
+        {a.map(function (item) {
+          return <li>{item}</li>;
+        })}
+      </div>
+      <div>
+        {/* 축약 2. */}
+        {/* element 가 component 로 mapping 될 수 있고 자주 일어나는 일이다. */}
+        {a.map((item) => (
+          <li>{item} 선수</li>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
