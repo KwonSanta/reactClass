cd2956eaacbf044a6c9f57c5d53b31224cf1aa3f
KwonSanta <KwonSanta@gmail.com>
2024-05-10 15:44:04 +0900

    이벤트 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button
+        onMouseLeave={() => console.log("mouse leaved")}
+        onMouseEnter={() => console.log("mouse entered")}
+        onClick={() => console.log("clicked")}
+      >
+        클릭1
+      </button>
+      <input type="text" onChange={() => console.log("changed")} />
+    </div>
+  );
 }
 
 export default App;
