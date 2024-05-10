e4b2e0c867c87ba1cfe90fd32ab2ef3fbfd628b6
KwonSanta <KwonSanta@gmail.com>
2024-05-10 16:01:00 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <button
-        onMouseLeave={() => console.log("mouse leaved")}
-        onMouseEnter={() => console.log("mouse entered")}
-        onClick={() => console.log("clicked")}
-      >
-        클릭1
-      </button>
-      <input type="text" onChange={() => console.log("changed")} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
