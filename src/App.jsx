0b4c4df908b30763f479056dc0308c5568006faa
KwonSanta <KwonSanta@gmail.com>
2024-05-10 15:19:18 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  return <div></div>;
-}
 function App(props) {
-  // props 의 이름은 lowerCamelCase 로 작성
-  // 예약어를 쓰지 말 것
-  return (
-    <div>
-      <label htmlFor="input1"></label>
-      <input id="input1" type="text" />
-      <MyComp number={3} pageNumber={3} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
