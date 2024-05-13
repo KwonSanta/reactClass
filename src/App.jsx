2b29bd460cd2a480888036348191bd837994abef
KwonSanta <KwonSanta@gmail.com>
2024-05-13 10:34:32 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { ArrowComp, MyComps, YourComp } from "./MyComps.jsx";
 
 function App(props) {
-  return (
-    <div>
-      <MyComps />
-      <YourComp />
-      <ArrowComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/MyComps.jsx
+++
@@ -1,9 +0,0 @@
-// react component 들
-export function MyComps() {
-  return <div>mycomp</div>;
-}
-
-export const YourComp = function () {
-  return <div>yourcomp</div>;
-};
-export const ArrowComp = () => <div>arrow function component</div>;
