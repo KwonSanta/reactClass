0478f719980f86871a452035e1f8363a495d1de2
KwonSanta <KwonSanta@gmail.com>
2024-05-13 11:11:41 +0900

    default export

--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,7 +1,2 @@
-const v1 = "korea";
-const v2 = 44;
-export const v3 = "seoul";
-
-// 한 번에 export
-// named export
-export { v1, v2 };
+const val = "hello";
+export default val;
---
+++ src/MyValues.jsx
@@ -0,0 +1,8 @@
+// named export
+export const v1 = "son";
+
+// default export
+// string, number, function, object, array 다 올수 있음.
+// 한 파일에서 하나만 가능
+export default "lee";
+// export default "kim"; // X
