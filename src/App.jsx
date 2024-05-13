3f9ac6b347757f9bcecf63396b3647a98d43c551
KwonSanta <KwonSanta@gmail.com>
2024-05-13 16:03:20 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [age, setAge] = useState("");
-  const [name, setName] = useState("");
-  const [country, setCountry] = useState("");
-  const [team, setTeam] = useState("");
-
-  // 필요시 여러 state 사용 가능
-
-  return (
-    <div>
-      <div>
-        <input
-          onChange={(e) => setAge(e.target.value)}
-          type="text"
-          placeholder={"나이"}
-        />
-      </div>
-      <div>
-        <input
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-          placeholder={"이름"}
-        />
-      </div>
-      <div>
-        <input
-          onChange={(e) => setCountry(e.target.value)}
-          type="text"
-          placeholder={"국가"}
-        />
-      </div>
-      <div>
-        <input
-          onChange={(e) => setTeam(e.target.value)}
-          type="text"
-          placeholder={"팀"}
-        />
-      </div>
-
-      <div>
-        {age}세 {name}은 {country}, {team} 소속입니다.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
