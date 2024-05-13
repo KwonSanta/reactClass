b1e07266f11abf4b198ce53fdb521d327f9e9991
KwonSanta <KwonSanta@gmail.com>
2024-05-13 10:27:43 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { action1, action2, action3, MyComp as YourComp } from "./MyComps.jsx";
-import { HereComp } from "./HereComp.jsx";
 
 function App(props) {
-  action1();
-  action2();
-  action3();
-  return (
-    <div>
-      <YourComp />
-      <HereComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
