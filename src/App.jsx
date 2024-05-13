7cc53fb32e797544d02adcd53a0707d1a9910ef4
KwonSanta <KwonSanta@gmail.com>
2024-05-13 12:29:49 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp1() {
-  const [val, setVal] = useState(0);
-
-  function updateVal() {
-    // react 는  re render 시점을 적절한 순간으로 선택(react 가 자체적으로 판단하여 실행)
-    setVal(val + 1);
-    setVal(val + 1);
-    setVal(val + 1);
-    console.log("val", val);
-  }
-  console.log("val", val);
-
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={updateVal}>증가</button>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp1 />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
