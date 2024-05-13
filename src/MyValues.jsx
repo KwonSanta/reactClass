142bc4c7b126ad8d21caca262258504c17a23fbb
KwonSanta <KwonSanta@gmail.com>
2024-05-13 11:13:54 +0900

    코드 리셋

--- src/MyValues.jsx
+++
@@ -1,8 +0,0 @@
-// named export
-export const v1 = "son";
-
-// default export
-// string, number, function, object, array 다 올수 있음.
-// 한 파일에서 하나만 가능
-export default "lee";
-// export default "kim"; // X
