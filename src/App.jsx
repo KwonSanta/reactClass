ea1292129ce5f35ca392325c17fe385748d76974
KwonSanta <KwonSanta@gmail.com>
2024-05-14 15:10:19 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("mount 될 떄");
-  }, []);
-  useEffect(() => {
-    console.log("count가 변경 때될 때");
-    // dependency 가 코드실행문 안에 포함되어서 무한루프가 되지 않도록 주의!!
-  }, [count]);
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>click</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
