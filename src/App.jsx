06d2dfb0d86467625b8a86c4b4c141744b337e8b
KwonSanta <KwonSanta@gmail.com>
2024-05-16 12:18:35 +0900

    js 객체가 json 으로 직렬화 됨 이 때 각 데이터타입이 사용됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  // json 데이터 타입
+  // 문자열, 수, 불리언, 객체, 배열, null
+
+  function handleClick1() {
+    const data = {
+      name: "son", // 문자열
+      age: 33, // 수
+      married: false, // boolean
+      team: {
+        // 객체
+        location: "london",
+        name: "토트넘",
+      },
+      item: ["phone", "shoes", "shirts"], // 배열
+      address: null, // null
+    };
+    axios.post("/api/main41/sub5", data);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>json 데이터와 요청</button>
+    </div>
+  );
 }
 
 export default App;
