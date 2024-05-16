5b4ef655e3a0e73fd09566e7d7cc93be2ff4a2cc
KwonSanta <KwonSanta@gmail.com>
2024-05-16 11:41:43 +0900

    input에 입력한값 버튼 클릭시 json으로 직렬화해서 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+
   function handleClick1() {
     // json 은 post 방식으로만
     const obj1 = { name: "son", age: 44 };
@@ -10,9 +13,41 @@ function App(props) {
     axios.post("/api/main41/sub1", obj1);
   }
 
+  function handleClick2() {
+    const obj2 = {
+      name: "lee",
+      age: 23,
+      // car: { carName: "model-3", carCompany: "tesla", carPrice: 6000 },
+      foods: ["apple", "banana", "pear"],
+    };
+    axios.post("/api/main41/sub2", obj2);
+  }
+
+  function handleClick3() {
+    // 변수명=프로퍼티명 -> 생략가능
+    // const data = {
+    //   name : name,
+    //   city : city
+    // }
+    // const data = {
+    //   name ,
+    //   city
+    // }
+    axios.post("/api/main41/sub3", { name, city });
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>요청 (json 데이터 포함)</button>
+      {/* todo : /api/main41/sub2 post with json data */}
+      <button onClick={handleClick2}>요청 (json 데이터 포함)</button>
+      <div>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <br />
+        <input type="text" onChange={(e) => setCity(e.target.value)} />
+        <br />
+        <button onClick={handleClick3}>요청 (input에 입력한 값)</button>
+      </div>
     </div>
   );
 }
