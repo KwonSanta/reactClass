a1e8229492ee5014d65b430da954557c0ae9663c
KwonSanta <KwonSanta@gmail.com>
2024-05-20 14:28:41 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  function handleButtonClick() {
-    // event bubbling 멈추기
-    e.stopPropagation();
-    console.log("버튼 클릭 됨.");
-  }
-
-  function handdleBoxClick() {
-    console.log("박스 클릭 됨.");
-  }
-
-  return (
-    <ChakraProvider>
-      <Box onClick={handdleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
-        <Button onClick={handleButtonClick}>클릭</Button>
-      </Box>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
