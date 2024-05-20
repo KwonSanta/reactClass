a38a071da4b3697767b58155ef97844dd3c6957f
KwonSanta <KwonSanta@gmail.com>
2024-05-20 14:01:19 +0900

    코드리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { ChakraProvider, useDisclosure, useToast } from "@chakra-ui/react";
+import React from "react";
 
 function App(props) {
-  // 안전하게 hook 은 컴포넌트 가장 상단에 순서대로 작성 할 것!!
-  const [count, setCount] = useState(0);
-  useEffect(() => {}, []);
-  const toast = useToast();
-  const { isOpen, onClose, onOpen } = useDisclosure();
-
-  // hook : use...로 시작하는 메소드들 (ex. useState, useEffect, useToast, ...)
-  // hook 사용시 if / for 내에서 사용하지 말 것 🔥🔥
-  // [EX] 작성하면 안되는 예시
-  if (true) {
-    const [text, setText] = useState("");
-  }
-  const [message, setMessage] = useState("");
-  return;
-  <ChakraProvider>
-    <div></div>;
-  </ChakraProvider>;
+  return <div></div>;
 }
 
 export default App;
