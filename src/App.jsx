379e67f1f2afb20928e56e140a52de971de55588
KwonSanta <KwonSanta@gmail.com>
2024-05-20 10:35:29 +0900

    차크라 ui : Flex, Spacer, Center

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Box, Center, ChakraProvider, Flex, Spacer } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
-        Lorem.
-      </Button>
+      <Flex justify={"space-between"}>
+        <Box bgColor={"blue"}>Lorem.</Box>
+        <Box bgColor={"orange"}>Animi.</Box>
+        <Box bgColor={"yellow"}>Assumenda?</Box>
+      </Flex>
+      <hr />
+      <Flex>
+        <Box bgColor={"gray"}>1</Box>
+        <Box bgColor={"gold"}>2</Box>
+        <Spacer /> {/* 두 element 사이를 최대로 공간을 잡음 */}
+        <Box bgColor={"green"}>3</Box>
+      </Flex>
+      <hr />
+      <Center>
+        <Box>lorem1</Box>
+      </Center>
+      <hr />
+      <Center w={{ base: "100%", md: "500px" }} bgColor={"linen"}>
+        <Box>contents</Box>
+      </Center>
     </ChakraProvider>
   );
 }
