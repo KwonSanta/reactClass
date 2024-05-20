1e935e0a85dc66bf107905c95ba05de13edc80f2
KwonSanta <KwonSanta@gmail.com>
2024-05-20 15:11:35 +0900

    file 전송
    [필수 코드]
    method="POST"
    encType="multipart/form-data"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <form action="/path" method="POST" encType="multipart/form-data">
+        이름 <input type="text" name="name" />
+        <br />
+        <input type="file" name="file" id="" />
+        <br />
+        <input type="submit" value="제출" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
